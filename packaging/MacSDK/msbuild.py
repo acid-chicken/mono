f24f8a3f20bb338060a839e0e750c80db60dad36
Restyled.io <commits@restyled.io>
2019-11-29 11:48:02 +0000

    Restyled by black

--- packaging/MacSDK/msbuild.py
+++ packaging/MacSDK/msbuild.py
@@ -1,22 +1,32 @@
 import fileinput
 
 
-class MSBuild (GitHubPackage):
+class MSBuild(GitHubPackage):
     def __init__(self):
-        GitHubPackage.__init__(self, 'mono', 'msbuild', '15',  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
-                               revision='e531c61da945ece8741d22f813bdf9da905d8d58')
+        GitHubPackage.__init__(
+            self,
+            "mono",
+            "msbuild",
+            "15",  # note: fix scripts/ci/run-test-mac-sdk.sh when bumping the version number
+            revision="e531c61da945ece8741d22f813bdf9da905d8d58",
+        )
 
     def build(self):
         try:
             self.sh(
-                './eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests')
+                "./eng/cibuild_bootstrapped_msbuild.sh --host_type mono --configuration Release --skip_tests"
+            )
         finally:
             self.sh(
-                'find artifacts stage1 -wholename \'*/log/*\' -type f -exec zip msbuild-bin-logs.zip {} \+')
+                "find artifacts stage1 -wholename '*/log/*' -type f -exec zip msbuild-bin-logs.zip {} \+"
+            )
 
     def install(self):
         # use the bootstrap msbuild as the system might not have one available!
-        self.sh('./stage1/mono-msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix=%s /p:Configuration=Release-MONO /p:IgnoreDiffFailure=true' % self.staged_prefix)
+        self.sh(
+            "./stage1/mono-msbuild/msbuild mono/build/install.proj /p:MonoInstallPrefix=%s /p:Configuration=Release-MONO /p:IgnoreDiffFailure=true"
+            % self.staged_prefix
+        )
 
 
 MSBuild()
