f597a3856abaa409d2bcaaaef64b032d03a7a1d0
Restyled.io <commits@restyled.io>
2019-12-04 20:47:53 +0000

    Restyled by astyle

--- mcs/class/corlib/Assembly/AssemblyInfo.cs
+++ mcs/class/corlib/Assembly/AssemblyInfo.cs
@@ -17,10 +17,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -60,11 +60,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyDelaySign (true)]
 #if MOBILE
-	[assembly: AssemblyKeyFile ("../silverlight.pub")]
+[assembly: AssemblyKeyFile ("../silverlight.pub")]
 #else
-	[assembly: AssemblyKeyFile ("../ecma.pub")]
-	[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
-	[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyKeyFile ("../ecma.pub")]
+[assembly: ComCompatibleVersion (1, 0, 3300, 0)]
+[assembly: AllowPartiallyTrustedCallers]
 #endif
 
 [assembly: AssemblyFileVersion (Consts.FxFileVersion)]
