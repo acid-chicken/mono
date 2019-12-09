ae9d20d3e074f5c4dcf6013409cd0304da5b87d4
Restyled.io <commits@restyled.io>
2019-12-09 14:48:33 +0000

    Restyled by autopep8

--- netcore/xunit-summary.py
+++ netcore/xunit-summary.py
@@ -9,7 +9,8 @@ import sys
 if len(sys.argv) < 1:
     print("Usage: xunit-summary.py <path to xunit results (*.xml)>")
     sys.exit(1)
-test_dir = sys.argv [1]
+test_dir = sys.argv[1]
+
 
 class AssemblyTestResults():
     def __init__(self, name, total, passed, failed, skipped, errors, time):
@@ -20,10 +21,12 @@ class AssemblyTestResults():
         self.skipped = skipped
         self.time = time
 
+
 class TestInfo():
     def __init__(self, name, time):
         self.name = name
         self.time = time
+
 
 test_assemblies = []
 test_items = []
@@ -35,17 +38,17 @@ for testfile in glob.glob(test_dir + "/*-xunit.xml"):
         if test_name is None:
             print("WARNING: %s has no tests!" % ntpath.basename(testfile))
             continue
-        test_assemblies.append(AssemblyTestResults(test_name, 
-            int(assembly.attrib["total"]), 
-            int(assembly.attrib["passed"]), 
-            int(assembly.attrib["failed"]), 
-            int(assembly.attrib["skipped"]), 
-            int(assembly.attrib["errors"]), 
-            float(assembly.attrib["time"])))
+        test_assemblies.append(AssemblyTestResults(test_name,
+                                                   int(assembly.attrib["total"]),
+                                                   int(assembly.attrib["passed"]),
+                                                   int(assembly.attrib["failed"]),
+                                                   int(assembly.attrib["skipped"]),
+                                                   int(assembly.attrib["errors"]),
+                                                   float(assembly.attrib["time"])))
         for collection in assembly.iter("collection"):
             for test in collection.iter("test"):
-                test_items.append(TestInfo(test.attrib["name"], 
-                    float(test.attrib["time"])))
+                test_items.append(TestInfo(test.attrib["name"],
+                                           float(test.attrib["time"])))
 
 test_assemblies.sort(key=lambda item: (item.failed, item.name), reverse=True)
 test_items.sort(key=lambda item: (item.time), reverse=True)
@@ -54,8 +57,9 @@ print("")
 print("")
 print("=" * 105)
 for t in test_assemblies:
-    #if t.failed > 0: # uncomment to list only test suits with failures
-        print("{0:<60}  Total:{1:<6}  Failed:{2:<6}  Time:{3} sec".format(t.name, t.total, t.failed, round(t.time, 1)))
+    # if t.failed > 0: # uncomment to list only test suits with failures
+    print("{0:<60}  Total:{1:<6}  Failed:{2:<6}  Time:{3} sec".format(
+        t.name, t.total, t.failed, round(t.time, 1)))
 print("=" * 105)
 print("")
 print("Total test suites:    %d" % len(test_assemblies))
@@ -63,7 +67,8 @@ print("Total tests run:      %d" % sum(x.total for x in test_assemblies))
 print("Total tests passed:   %d" % sum(x.passed for x in test_assemblies))
 print("Total tests failed:   %d" % sum(x.failed for x in test_assemblies))
 print("Total tests skipped:  %d" % sum(x.skipped for x in test_assemblies))
-print("Total duration:       %d min" % (sum(x.time for x in test_assemblies) / 60))
+print("Total duration:       %d min" %
+      (sum(x.time for x in test_assemblies) / 60))
 print("")
 print("")
 print("Top 20 slowest tests:")
