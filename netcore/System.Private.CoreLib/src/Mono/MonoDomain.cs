df10435cd3d15d41bb1d7ae8d421f3c515fb067c
Restyled.io <commits@restyled.io>
2019-12-11 23:48:09 +0000

    Restyled by clang-format

--- netcore/System.Private.CoreLib/src/Mono/MonoDomain.cs
+++ netcore/System.Private.CoreLib/src/Mono/MonoDomain.cs
@@ -6,36 +6,28 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-namespace Mono
-{
-[StructLayout (LayoutKind.Sequential)]
-internal sealed partial class MonoDomain
-{
+namespace Mono {
+[StructLayout(LayoutKind.Sequential)]
+internal sealed partial class MonoDomain {
 #pragma warning disable 169
-    #region Sync with object-internals.h
-    IntPtr _mono_app_domain;
-    #endregion
+#region Sync with object - internals.h
+  IntPtr _mono_app_domain;
+#endregion
 #pragma warning restore 169
 
-    public event AssemblyLoadEventHandler AssemblyLoad;
+  public event AssemblyLoadEventHandler AssemblyLoad;
 
-    private void DoAssemblyLoad (Assembly assembly)
-    {
-        return; /* FIXME */
-    }
+  private void DoAssemblyLoad(Assembly assembly) { return; /* FIXME */ }
 
-    internal Assembly? DoTypeResolve (string name)
-    {
-        return null;
-    }
+  internal Assembly? DoTypeResolve(string name) { return null; }
 
-    internal Assembly? DoTypeBuilderResolve (System.Reflection.Emit.TypeBuilder tb)
-    {
-        return null;
-    }
+  internal Assembly?
+  DoTypeBuilderResolve(System.Reflection.Emit.TypeBuilder tb) {
+    return null;
+  }
 
-    public event UnhandledExceptionEventHandler UnhandledException;
+  public event UnhandledExceptionEventHandler UnhandledException;
 
-    public event EventHandler ProcessExit;
+  public event EventHandler ProcessExit;
 }
 }
