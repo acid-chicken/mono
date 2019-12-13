48ac546a7c2904c18094dc82e58cdcf7e9aa4138
Restyled.io <commits@restyled.io>
2019-12-13 11:48:21 +0000

    Restyled by astyle

--- mcs/class/referencesource/System.ServiceModel/System/ServiceModel/XmlUtil.cs
+++ mcs/class/referencesource/System.ServiceModel/System/ServiceModel/XmlUtil.cs
@@ -3,119 +3,119 @@
 //------------------------------------------------------------
 namespace System.ServiceModel
 {
-    using System.Runtime;
-    using System.Xml;
+using System.Runtime;
+using System.Xml;
 
-    static class XmlUtil
+static class XmlUtil
+{
+    public const string XmlNs = "http://www.w3.org/XML/1998/namespace";
+    public const string XmlNsNs = "http://www.w3.org/2000/xmlns/";
+
+    public static string GetXmlLangAttribute(XmlReader reader)
     {
-        public const string XmlNs = "http://www.w3.org/XML/1998/namespace";
-        public const string XmlNsNs = "http://www.w3.org/2000/xmlns/";
-
-        public static string GetXmlLangAttribute(XmlReader reader)
+        string xmlLang = null;
+        if (reader.MoveToAttribute("lang", XmlNs))
         {
-            string xmlLang = null;
-            if (reader.MoveToAttribute("lang", XmlNs))
-            {
-                xmlLang = reader.Value;
-                reader.MoveToElement();
-            }
-
-            if (xmlLang == null)
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new XmlException(SR.GetString(SR.XmlLangAttributeMissing)));
-
-            return xmlLang;
+            xmlLang = reader.Value;
+            reader.MoveToElement();
         }
 
-        // FIX for 7455
-        public static bool IsTrue(string booleanValue)
-        {
-            if (string.IsNullOrEmpty(booleanValue))
-            {
-                return false;
-            }
+        if (xmlLang == null)
+            throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new XmlException(SR.GetString(SR.XmlLangAttributeMissing)));
 
-            return XmlConvert.ToBoolean(booleanValue);
+        return xmlLang;
+    }
+
+    // FIX for 7455
+    public static bool IsTrue(string booleanValue)
+    {
+        if (string.IsNullOrEmpty(booleanValue))
+        {
+            return false;
         }
 
-        public static void ReadContentAsQName(XmlReader reader, out string localName, out string ns)
+        return XmlConvert.ToBoolean(booleanValue);
+    }
+
+    public static void ReadContentAsQName(XmlReader reader, out string localName, out string ns)
+    {
+        ParseQName(reader, reader.ReadContentAsString(), out localName, out ns);
+    }
+
+    public static bool IsWhitespace(char ch)
+    {
+        return (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n');
+    }
+
+    public static string TrimEnd(string s)
+    {
+        int i;
+        for (i = s.Length; i > 0 && IsWhitespace(s[i - 1]); i--);
+
+        if (i != s.Length)
         {
-            ParseQName(reader, reader.ReadContentAsString(), out localName, out ns);
+            return s.Substring(0, i);
         }
 
-        public static bool IsWhitespace(char ch)
+        return s;
+    }
+
+    public static string TrimStart(string s)
+    {
+        int i;
+        for (i = 0; i < s.Length && IsWhitespace(s[i]); i++);
+
+        if (i != 0)
         {
-            return (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n');
+            return s.Substring(i);
         }
 
-        public static string TrimEnd(string s)
+        return s;
+    }
+
+    public static string Trim(string s)
+    {
+        int i;
+        for (i = 0; i < s.Length && IsWhitespace(s[i]); i++);
+
+        if (i >= s.Length)
         {
-            int i;
-            for (i = s.Length; i > 0 && IsWhitespace(s[i - 1]); i--);
-
-            if (i != s.Length)
-            {
-                return s.Substring(0, i);
-            }
-
-            return s;
+            return string.Empty;
         }
 
-        public static string TrimStart(string s)
-        {
-            int i;
-            for (i = 0; i < s.Length && IsWhitespace(s[i]); i++);
-
-            if (i != 0)
-            {
-                return s.Substring(i);
-            }
-
-            return s;
-        }
-
-        public static string Trim(string s)
-        {
-            int i;
-            for (i = 0; i < s.Length && IsWhitespace(s[i]); i++);
-
-            if (i >= s.Length)
-            {
-                return string.Empty;
-            }
-
-            int j;
-            for (j = s.Length; j > 0 && IsWhitespace(s[j - 1]); j--);
+        int j;
+        for (j = s.Length; j > 0 && IsWhitespace(s[j - 1]); j--);
 
 #if !MONO
-            Fx.Assert(j > i, "Logic error in XmlUtil.Trim().");
+        Fx.Assert(j > i, "Logic error in XmlUtil.Trim().");
 #endif
 
-            if (i != 0 || j != s.Length)
-            {
-                return s.Substring(i, j - i);
-            }
-            return s;
-        }
-
-        public static void ParseQName(XmlReader reader, string qname, out string localName, out string ns)
+        if (i != 0 || j != s.Length)
         {
-            int index = qname.IndexOf(':');
-            string prefix;
-            if (index < 0)
-            {
-                prefix = "";
-                localName = TrimStart(TrimEnd(qname));
-            }
-            else
-            {
-                if (index == qname.Length - 1)
-                    throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new XmlException(SR.GetString(SR.InvalidXmlQualifiedName, qname)));
-                prefix = TrimStart(qname.Substring(0, index));
-                localName = TrimEnd(qname.Substring(index + 1));
-            }
-            ns = reader.LookupNamespace(prefix);
-            if (ns == null)
-                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new XmlException(SR.GetString(SR.UnboundPrefixInQName, qname)));
+            return s.Substring(i, j - i);
         }
+        return s;
     }
+
+    public static void ParseQName(XmlReader reader, string qname, out string localName, out string ns)
+    {
+        int index = qname.IndexOf(':');
+        string prefix;
+        if (index < 0)
+        {
+            prefix = "";
+            localName = TrimStart(TrimEnd(qname));
+        }
+        else
+        {
+            if (index == qname.Length - 1)
+                throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new XmlException(SR.GetString(SR.InvalidXmlQualifiedName, qname)));
+            prefix = TrimStart(qname.Substring(0, index));
+            localName = TrimEnd(qname.Substring(index + 1));
+        }
+        ns = reader.LookupNamespace(prefix);
+        if (ns == null)
+            throw DiagnosticUtility.ExceptionUtility.ThrowHelperError(new XmlException(SR.GetString(SR.UnboundPrefixInQName, qname)));
+    }
+}
 }
