64b7ce9d3070d1f7097f5e866561beb3fe6dcb7d
Restyled.io <commits@restyled.io>
2020-01-28 15:45:14 +0000

    Restyled by astyle

--- mcs/class/System.Windows.Forms/System.Windows.Forms/ContextMenuStrip.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/ContextMenuStrip.cs
@@ -8,10 +8,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -30,48 +30,51 @@ using System.Runtime.InteropServices;
 
 namespace System.Windows.Forms
 {
-	[ComVisible (true)]
-	[ClassInterface (ClassInterfaceType.AutoDispatch)]
-	[DefaultEvent ("Opening")]
-	public class ContextMenuStrip : ToolStripDropDownMenu
-	{
-		internal Control AssociatedControl;
+[ComVisible (true)]
+[ClassInterface (ClassInterfaceType.AutoDispatch)]
+[DefaultEvent ("Opening")]
+public class ContextMenuStrip : ToolStripDropDownMenu
+{
+    internal Control AssociatedControl;
 
-		#region Public Construtors
-		public ContextMenuStrip () : base ()
-		{
-		}
-		
-		public ContextMenuStrip (IContainer container) : this ()
-		{
-			// TODO: handle `container` argument
-		}
-		#endregion
+    #region Public Construtors
+    public ContextMenuStrip () : base ()
+    {
+    }
 
-		#region Public Properties
-		[Browsable (false)]
-		[DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
-		public Control SourceControl { get; protected set; }
-		#endregion
+    public ContextMenuStrip (IContainer container) : this ()
+    {
+        // TODO: handle `container` argument
+    }
+    #endregion
 
-		#region Protected Methods
-		protected override void Dispose (bool disposing)
-		{
-			base.Dispose (disposing);
-		}
+    #region Public Properties
+    [Browsable (false)]
+    [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+    public Control SourceControl {
+        get;
+        protected set;
+    }
+    #endregion
 
-		protected override void SetVisibleCore (bool visible)
-		{
-			base.SetVisibleCore (visible);
-			if (visible)
-				XplatUI.SetTopmost (this.Handle, true);
-		}
+    #region Protected Methods
+    protected override void Dispose (bool disposing)
+    {
+        base.Dispose (disposing);
+    }
 
-		protected override void SetOwnerControl (Control control)
-		{
-			base.SetOwnerControl (control);
-			SourceControl = control;
-		}
-		#endregion
-	}
+    protected override void SetVisibleCore (bool visible)
+    {
+        base.SetVisibleCore (visible);
+        if (visible)
+            XplatUI.SetTopmost (this.Handle, true);
+    }
+
+    protected override void SetOwnerControl (Control control)
+    {
+        base.SetOwnerControl (control);
+        SourceControl = control;
+    }
+    #endregion
+}
 }
