dfe4530f5f87c60736888c710584860e641c4a74
Restyled.io <commits@restyled.io>
2020-02-17 16:37:33 +0000

    Restyled by clang-format

--- mono/tests/bug-gh-17285.cs
+++ mono/tests/bug-gh-17285.cs
@@ -1,34 +1,26 @@
 using System;
 
-namespace ConsoleApp2
-{
-class Program
-{
-    class C
-    {
-        public void f(int i1, int i2, int i3, int i4, int i5, int i6, int i7)
-        {
-            throw new Exception("exception from f()");
-        }
+namespace ConsoleApp2 {
+class Program {
+  class C {
+    public void f(int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
+      throw new Exception("exception from f()");
     }
+  }
 
-    // If this test succeeds, it should run to completion. If it fails,
-    // mono's exception handler will restore an incorrect stack pointer
-    // when executing the exception handler, which will cause the runtime
-    // to later crash after returning to a bogus address.
-    static void Main()
-    {
-        try
-        {
-            C c = new C();
-            int i1 = 0, i2 = 0, i3 = 0, i4 = 0, i5 = 0, i6 = 0, i7 = 0;
-            c.f(i1, i2, i3, i4, i5, i6, i7);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("caught: " + e);
-        }
+  // If this test succeeds, it should run to completion. If it fails,
+  // mono's exception handler will restore an incorrect stack pointer
+  // when executing the exception handler, which will cause the runtime
+  // to later crash after returning to a bogus address.
+  static void Main() {
+    try {
+      C c = new C();
+      int i1 = 0, i2 = 0, i3 = 0, i4 = 0, i5 = 0, i6 = 0, i7 = 0;
+      c.f(i1, i2, i3, i4, i5, i6, i7);
+    } catch (Exception e) {
+      Console.WriteLine("caught: " + e);
     }
+  }
 }
 }
 
@@ -42,9 +34,9 @@ CFA: [4] def_cfa_offset: 0x20
 CFA: [50] def_cfa_offset: 0x28
 CFA: [52] def_cfa_offset: 0x30
 CFA: [59] def_cfa_offset: 0x20
-LLVM Method void ConsoleApp2.Program:Main () emitted at 0x41a5ebd0 to 0x41a5ec75 (code length 165) [gh-issue-17285.exe]
-JitInfo EH clause 0 flags 0 try 21-32 handler 6c-b6abe448
-JitInfo EH clause 1 flags 0 try 3f-59 handler 6c-b6abe448
+LLVM Method void ConsoleApp2.Program:Main () emitted at 0x41a5ebd0 to 0x41a5ec75
+(code length 165) [gh-issue-17285.exe] JitInfo EH clause 0 flags 0 try 21-32
+handler 6c-b6abe448 JitInfo EH clause 1 flags 0 try 3f-59 handler 6c-b6abe448
 
 
    0:	48 83 ec 18          	sub    $0x18,%rsp
