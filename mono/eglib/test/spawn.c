fca8faeb223746906625462eca239e2750c9414c
Restyled.io <commits@restyled.io>
2020-02-25 20:30:18 +0000

    Restyled by clang-format

--- mono/eglib/test/spawn.c
+++ mono/eglib/test/spawn.c
@@ -1,7 +1,7 @@
 #include <config.h>
 #include <glib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -13,68 +13,65 @@
 #define close _close
 #endif
 
-static RESULT
-test_spawn_sync (void)
-{
-    gchar *out;
-    gchar *err;
-    gint status = -1;
-    GError *gerror = NULL;
+static RESULT test_spawn_sync(void) {
+  gchar *out;
+  gchar *err;
+  gint status = -1;
+  GError *gerror = NULL;
 
-    if (!g_spawn_command_line_sync ("ls", &out, &err, &status, &gerror))
-        return FAILED ("Error executing 'ls'");
+  if (!g_spawn_command_line_sync("ls", &out, &err, &status, &gerror))
+    return FAILED("Error executing 'ls'");
 
-    if (status != 0)
-        return FAILED ("Status is %d", status);
+  if (status != 0)
+    return FAILED("Status is %d", status);
 
-    if (out == NULL || strlen (out) == 0)
-        return FAILED ("Didn't get any output from ls!?");
+  if (out == NULL || strlen(out) == 0)
+    return FAILED("Didn't get any output from ls!?");
 
-    g_free (out);
-    g_free (err);
-    return OK;
+  g_free(out);
+  g_free(err);
+  return OK;
 }
 
-static RESULT
-test_spawn_async (void)
-{
-    /*
-    gboolean
-    g_spawn_async_with_pipes (const gchar *working_directory,
-    		gchar **argv,
-    		gchar **envp,
-    		GSpawnFlags flags,
-    		GSpawnChildSetupFunc child_setup,
-    		gpointer user_data,
-    		GPid *child_pid,
-    		gint *standard_input,
-    		gint *standard_output,
-    		gint *standard_error,
-    		GError **gerror) */
-    char *argv [15];
-    int stdout_fd = -1;
-    char buffer [512];
-    GPid child_pid = 0;
+static RESULT test_spawn_async(void) {
+  /*
+  gboolean
+  g_spawn_async_with_pipes (const gchar *working_directory,
+              gchar **argv,
+              gchar **envp,
+              GSpawnFlags flags,
+              GSpawnChildSetupFunc child_setup,
+              gpointer user_data,
+              GPid *child_pid,
+              gint *standard_input,
+              gint *standard_output,
+              gint *standard_error,
+              GError **gerror) */
+  char *argv[15];
+  int stdout_fd = -1;
+  char buffer[512];
+  GPid child_pid = 0;
 
-    memset (argv, 0, 15 * sizeof (char *));
-    argv [0] = (char*)"ls";
-    if (!g_spawn_async_with_pipes (NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, &child_pid, NULL, &stdout_fd, NULL, NULL))
-        return FAILED ("1 Failed to run ls");
-    if (child_pid == 0)
-        return FAILED ("2 child pid not returned");
-    if (stdout_fd == -1)
-        return FAILED ("3 out fd is -1");
+  memset(argv, 0, 15 * sizeof(char *));
+  argv[0] = (char *)"ls";
+  if (!g_spawn_async_with_pipes(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL,
+                                NULL, &child_pid, NULL, &stdout_fd, NULL, NULL))
+    return FAILED("1 Failed to run ls");
+  if (child_pid == 0)
+    return FAILED("2 child pid not returned");
+  if (stdout_fd == -1)
+    return FAILED("3 out fd is -1");
 
-    while (read (stdout_fd, buffer, 512) > 0);
-    close (stdout_fd);
+  while (read(stdout_fd, buffer, 512) > 0)
+    ;
+  close(stdout_fd);
 
-    return OK;
+  return OK;
 }
 
-static Test spawn_tests [] = {
+static Test spawn_tests[] = {
     {"g_shell_spawn_sync", test_spawn_sync},
     {"g_shell_spawn_async_with_pipes", test_spawn_async},
-    {NULL, NULL}
-};
+    {NULL, NULL}};
 
 DEFINE_TEST_GROUP_INIT(spawn_tests_init, spawn_tests)
