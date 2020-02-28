0f2852af8113dfced1bda366d16508ac96623ebd
Restyled.io <commits@restyled.io>
2020-02-28 18:30:41 +0000

    Restyled by black

--- packaging/MacSDKRelease/mono-extensions.py
+++ packaging/MacSDKRelease/mono-extensions.py
@@ -2,13 +2,15 @@ from bockbuild.package import Package
 
 
 class MonoExtensionsPackage(Package):
-
     def __init__(self):
-        Package.__init__(self, 'mono-extensions', None,
-                         sources=['git@github.com:xamarin/mono-extensions.git'],
-                         revision='ceae34ed8c7c6d1134f9cf643167d540a6204f86'
-                         )
-        self.source_dir_name = 'mono-extensions'
+        Package.__init__(
+            self,
+            "mono-extensions",
+            None,
+            sources=["git@github.com:xamarin/mono-extensions.git"],
+            revision="ceae34ed8c7c6d1134f9cf643167d540a6204f86",
+        )
+        self.source_dir_name = "mono-extensions"
 
     def build(self):
         pass
