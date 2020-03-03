6a0df95ad04b380730486eeae03c528498b00027
Restyled.io <commits@restyled.io>
2020-03-03 20:32:35 +0000

    Restyled by dotnet-format

--- mcs/class/System.ComponentModel.DataAnnotations/corefx/CountPropertyHelper.cs
+++ mcs/class/System.ComponentModel.DataAnnotations/corefx/CountPropertyHelper.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Diagnostics;
 using System.Reflection;
 
-internal static class CountPropertyHelper {
-  public static bool TryGetCount(object value, out int count) {
-    Debug.Assert(value != null);
+internal static class CountPropertyHelper
+{
+    public static bool TryGetCount(object value, out int count)
+    {
+        Debug.Assert(value != null);
 
-    if (value is ICollection collection) {
-      count = collection.Count;
-      return true;
+        if (value is ICollection collection)
+        {
+            count = collection.Count;
+            return true;
+        }
+
+        PropertyInfo property = value.GetType().GetRuntimeProperty("Count");
+        if (property != null && property.CanRead &&
+            property.PropertyType == typeof(int))
+        {
+            count = (int)property.GetValue(value);
+            return true;
+        }
+
+        count = -1;
+        return false;
     }
-
-    PropertyInfo property = value.GetType().GetRuntimeProperty("Count");
-    if (property != null && property.CanRead &&
-        property.PropertyType == typeof (int)) {
-      count = (int) property.GetValue(value);
-      return true;
-    }
-
-    count = -1;
-    return false;
-  }
 }
