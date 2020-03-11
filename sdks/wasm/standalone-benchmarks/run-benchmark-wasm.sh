76b415831ef9594ccebb28c1ae56e08aedb76f3b
Restyled.io <commits@restyled.io>
2020-03-11 20:30:08 +0000

    Restyled by shellharden

--- sdks/wasm/standalone-benchmarks/run-benchmark-wasm.sh
+++ sdks/wasm/standalone-benchmarks/run-benchmark-wasm.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 set -u
-function realpath { echo $(cd $(dirname ${1}); pwd)/$(basename ${1}); }
+function realpath { echo "$(cd "$(dirname "${1}")"; pwd)/$(basename "${1}")"; }
 
 BENCHMARK=""
 while (( "$#" )); do
@@ -20,25 +20,25 @@ while (( "$#" )); do
       exit 1
       ;;
     *)
-      BENCHMARK=$(basename $1)
+      BENCHMARK=$(basename "$1")
       shift
       ;;
   esac
 done
 
 
-if [ ! -z ${MONO_PACKAGE+x} ]; then
+if [ ! -z "${MONO_PACKAGE+x}" ]; then
     ZIP_DIR=${RUNTIME_DIR:=$(basename ${MONO_PACKAGE%.*})}
-    unzip -u -d $ZIP_DIR $MONO_PACKAGE "builds/**"
+    unzip -u -d "$ZIP_DIR" "$MONO_PACKAGE" "builds/**"
     RUNTIME_DIR=$ZIP_DIR/builds
 fi
 RUNTIME_DIR=${RUNTIME_DIR:="../builds"}
 
 PACKAGER=$(realpath ../packager.exe)
-SRC_DIR=$(realpath $BENCHMARK/bin/Debug/netcoreapp2.1)
+SRC_DIR=$(realpath "$BENCHMARK"/bin/Debug/netcoreapp2.1)
 TEMPLATE_PATH=$(realpath ../runtime.js)
 
 echo "Using runtime $RUNTIME_DIR"
-dotnet build $BENCHMARK
-mono --debug $PACKAGER --wasm-runtime-path=$RUNTIME_DIR --out=wasm/$BENCHMARK $SRC_DIR/$BENCHMARK.dll --template=$TEMPLATE_PATH
-node test-runner.js $BENCHMARK wasm/$BENCHMARK
+dotnet build "$BENCHMARK"
+mono --debug "$PACKAGER" --wasm-runtime-path="$RUNTIME_DIR" --out=wasm/"$BENCHMARK" "$SRC_DIR/$BENCHMARK".dll --template="$TEMPLATE_PATH"
+node test-runner.js "$BENCHMARK" wasm/"$BENCHMARK"
