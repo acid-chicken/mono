9c152ed28c6a807366864077d3415ffc0f14037f
Restyled.io <commits@restyled.io>
2020-03-24 12:15:22 +0000

    Restyled by astyle

--- mono/mini/interp/transform.h
+++ mono/mini/interp/transform.h
@@ -16,9 +16,9 @@ typedef struct InterpInst InterpInst;
 
 typedef struct
 {
-	MonoClass *klass;
-	unsigned char type;
-	unsigned char flags;
+    MonoClass *klass;
+    unsigned char type;
+    unsigned char flags;
 } StackInfo;
 
 #define STACK_VALUE_NONE 0
@@ -30,119 +30,119 @@ typedef struct
 // StackValue contains data to construct an InterpInst that is equivalent with the contents
 // of the stack slot / local / argument.
 typedef struct {
-	// Indicates the type of the stored information. It can be a local, argument or a constant
-	int type;
-	// Holds the local index or the actual constant value
-	union {
-		int local;
-		int arg;
-		gint32 i;
-		gint64 l;
-	};
+    // Indicates the type of the stored information. It can be a local, argument or a constant
+    int type;
+    // Holds the local index or the actual constant value
+    union {
+        int local;
+        int arg;
+        gint32 i;
+        gint64 l;
+    };
 } StackValue;
 
 typedef struct
 {
-	// This indicates what is currently stored in this stack slot. This can be a constant
-	// or the copy of a local / argument.
-	StackValue val;
-	// The instruction that pushed this stack slot. If ins is null, we can't remove the usage
-	// of the stack slot, because we can't clear the instruction that set it.
-	InterpInst *ins;
+    // This indicates what is currently stored in this stack slot. This can be a constant
+    // or the copy of a local / argument.
+    StackValue val;
+    // The instruction that pushed this stack slot. If ins is null, we can't remove the usage
+    // of the stack slot, because we can't clear the instruction that set it.
+    InterpInst *ins;
 } StackContentInfo;
 
 struct InterpInst {
-	guint16 opcode;
-	InterpInst *next, *prev;
-	// If this is -1, this instruction is not logically associated with an IL offset, it is
-	// part of the IL instruction associated with the previous interp instruction.
-	int il_offset;
-	guint32 flags;
-	guint16 data [MONO_ZERO_LEN_ARRAY];
+    guint16 opcode;
+    InterpInst *next, *prev;
+    // If this is -1, this instruction is not logically associated with an IL offset, it is
+    // part of the IL instruction associated with the previous interp instruction.
+    int il_offset;
+    guint32 flags;
+    guint16 data [MONO_ZERO_LEN_ARRAY];
 };
 
 typedef struct {
-	guint8 *ip;
-	GSList *preds;
-	GSList *seq_points;
-	SeqPoint *last_seq_point;
+    guint8 *ip;
+    GSList *preds;
+    GSList *seq_points;
+    SeqPoint *last_seq_point;
 
-	// This will hold a list of last sequence points of incoming basic blocks
-	SeqPoint **pred_seq_points;
-	guint num_pred_seq_points;
+    // This will hold a list of last sequence points of incoming basic blocks
+    SeqPoint **pred_seq_points;
+    guint num_pred_seq_points;
 } InterpBasicBlock;
 
 typedef enum {
-	RELOC_SHORT_BRANCH,
-	RELOC_LONG_BRANCH,
-	RELOC_SWITCH
+    RELOC_SHORT_BRANCH,
+    RELOC_LONG_BRANCH,
+    RELOC_SWITCH
 } RelocType;
 
 typedef struct {
-	RelocType type;
-	/* In the interpreter IR */
-	int offset;
-	/* In the IL code */
-	int target;
+    RelocType type;
+    /* In the interpreter IR */
+    int offset;
+    /* In the IL code */
+    int target;
 } Reloc;
 
 typedef struct {
-	MonoType *type;
-	int mt;
-	int flags;
-	int indirects;
-	int offset;
+    MonoType *type;
+    int mt;
+    int flags;
+    int indirects;
+    int offset;
 } InterpLocal;
 
 typedef struct
 {
-	MonoMethod *method;
-	MonoMethod *inlined_method;
-	MonoMethodHeader *header;
-	InterpMethod *rtm;
-	const unsigned char *il_code;
-	const unsigned char *ip;
-	const unsigned char *in_start;
-	InterpInst *last_ins, *first_ins;
-	int code_size;
-	int *in_offsets;
-	int current_il_offset;
-	StackInfo **stack_state;
-	int *stack_height;
-	int *vt_stack_size;
-	unsigned char *is_bb_start;
-	unsigned short *new_code;
-	unsigned short *new_code_end;
-	unsigned int max_code_size;
-	StackInfo *stack;
-	StackInfo *sp;
-	unsigned int max_stack_height;
-	unsigned int stack_capacity;
-	unsigned int vt_sp;
-	unsigned int max_vt_sp;
-	unsigned int total_locals_size;
-	InterpLocal *locals;
-	unsigned int locals_size;
-	unsigned int locals_capacity;
-	int n_data_items;
-	int max_data_items;
-	void **data_items;
-	GHashTable *data_hash;
+    MonoMethod *method;
+    MonoMethod *inlined_method;
+    MonoMethodHeader *header;
+    InterpMethod *rtm;
+    const unsigned char *il_code;
+    const unsigned char *ip;
+    const unsigned char *in_start;
+    InterpInst *last_ins, *first_ins;
+    int code_size;
+    int *in_offsets;
+    int current_il_offset;
+    StackInfo **stack_state;
+    int *stack_height;
+    int *vt_stack_size;
+    unsigned char *is_bb_start;
+    unsigned short *new_code;
+    unsigned short *new_code_end;
+    unsigned int max_code_size;
+    StackInfo *stack;
+    StackInfo *sp;
+    unsigned int max_stack_height;
+    unsigned int stack_capacity;
+    unsigned int vt_sp;
+    unsigned int max_vt_sp;
+    unsigned int total_locals_size;
+    InterpLocal *locals;
+    unsigned int locals_size;
+    unsigned int locals_capacity;
+    int n_data_items;
+    int max_data_items;
+    void **data_items;
+    GHashTable *data_hash;
 #ifdef ENABLE_EXPERIMENT_TIERED
-	GHashTable *patchsite_hash;
+    GHashTable *patchsite_hash;
 #endif
-	int *clause_indexes;
-	gboolean gen_sdb_seq_points;
-	GPtrArray *seq_points;
-	InterpBasicBlock **offset_to_bb;
-	InterpBasicBlock *entry_bb;
-	MonoMemPool     *mempool;
-	GList *basic_blocks;
-	GPtrArray *relocs;
-	gboolean verbose_level;
-	GArray *line_numbers;
-	gboolean prof_coverage;
-	MonoProfilerCoverageInfo *coverage_info;
+    int *clause_indexes;
+    gboolean gen_sdb_seq_points;
+    GPtrArray *seq_points;
+    InterpBasicBlock **offset_to_bb;
+    InterpBasicBlock *entry_bb;
+    MonoMemPool     *mempool;
+    GList *basic_blocks;
+    GPtrArray *relocs;
+    gboolean verbose_level;
+    GArray *line_numbers;
+    gboolean prof_coverage;
+    MonoProfilerCoverageInfo *coverage_info;
 } TransformData;
 
 #define STACK_TYPE_I4 0
