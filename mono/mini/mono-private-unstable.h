108467551ff53bc8545d5bf30245b80f15da7a1b
Restyled.io <commits@restyled.io>
2020-03-24 00:15:55 +0000

    Restyled by clang-format

--- mono/mini/mono-private-unstable.h
+++ mono/mini/mono-private-unstable.h
@@ -3,17 +3,14 @@
  *
  * Private unstable APIs.
  *
- * WARNING: The declarations and behavior of functions in this header are NOT STABLE and can be modified or removed at
- * any time.
+ * WARNING: The declarations and behavior of functions in this header are NOT
+ * STABLE and can be modified or removed at any time.
  *
  */
-
 
 #ifndef __MONO_JIT_MONO_PRIVATE_UNSTABLE_H__
 #define __MONO_JIT_MONO_PRIVATE_UNSTABLE_H__
 
 #include <mono/utils/mono-publib.h>
-
-
 
 #endif /*__MONO_JIT_MONO_PRIVATE_UNSTABLE_H__*/
