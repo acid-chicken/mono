33f15bdc86846102bafd03f0fd2ddb938b716c69
Restyled.io <commits@restyled.io>
2020-04-10 08:36:33 +0000

    Restyled by shfmt

--- sdks/wasm/package-update/download-packages.sh
+++ sdks/wasm/package-update/download-packages.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-while (("$#"));
-do 
-    key="${1}"
-    
-    case $key in
-    -r|--runtime)
-        RUNTIME="$2"
-        shift 2
-        ;;
-    -f|--filepath)
-        FILEPATH="$2"
-        shift 2
-        ;;
-    -u|--url)
-        URL="$2"
-        shift 2
-        ;;
+while (("$#")); do
+  key="${1}"
+
+  case $key in
+    -r | --runtime)
+      RUNTIME="$2"
+      shift 2
+      ;;
+    -f | --filepath)
+      FILEPATH="$2"
+      shift 2
+      ;;
+    -u | --url)
+      URL="$2"
+      shift 2
+      ;;
     *)
-        shift
-        ;;
-    esac
+      shift
+      ;;
+  esac
 done
 
 PACKAGE_PATH=$HOME/.nuget/packages/microsoft.aspnetcore.components.webassembly.runtime/$RUNTIME/tools/dotnetwasm
 
-if [ -z "$FILEPATH" ]
-then
-    wget -O wasm-package.zip "$URL"
-    unzip wasm-package.zip -d wasm-package/
-    FILEPATH=wasm-package/
+if [ -z "$FILEPATH" ]; then
+  wget -O wasm-package.zip "$URL"
+  unzip wasm-package.zip -d wasm-package/
+  FILEPATH=wasm-package/
 fi
 
 rm -r "$PACKAGE_PATH"/bcl
