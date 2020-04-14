41a026ca16bbb3bc76d3491af3b748f8af5f13fb
Restyled.io <commits@restyled.io>
2020-04-14 12:36:41 +0000

    Restyled by astyle

--- mono/mini/mini-windows.c
+++ mono/mini/mini-windows.c
@@ -69,9 +69,9 @@
 typedef void (*handler)(void);
 
 typedef struct {
-	const char * cmd;
-	const int cmd_len;
-	handler handler;
+    const char * cmd;
+    const int cmd_len;
+    handler handler;
 } HandlerItem;
 
 #if G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT)
@@ -86,14 +86,14 @@ static void
 atexit_wait_keypress (void)
 {
 
-	fflush (stdin);
+    fflush (stdin);
 
-	printf ("Press any key to continue . . . ");
-	fflush (stdout);
+    printf ("Press any key to continue . . . ");
+    fflush (stdout);
 
-	_getch ();
+    _getch ();
 
-	return;
+    return;
 }
 
 /**
@@ -104,8 +104,8 @@ atexit_wait_keypress (void)
 static void
 install_atexit_wait_keypress (void)
 {
-	atexit (atexit_wait_keypress);
-	return;
+    atexit (atexit_wait_keypress);
+    return;
 }
 
 #else
@@ -118,14 +118,15 @@ install_atexit_wait_keypress (void)
 static void
 install_atexit_wait_keypress (void)
 {
-	return;
+    return;
 }
 
 #endif /* G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT) */
 
 // Table describing handlers that can be installed at process startup. Adding a new handler can be done by adding a new item to the table together with an install handler function.
 const HandlerItem g_handler_items[] = { { MONO_HANDLER_ATEXIT_WAIT_KEYPRESS, MONO_HANDLER_ATEXIT_WAIT_KEYPRESS_LEN, install_atexit_wait_keypress },
-					{ NULL, 0, NULL } };
+    { NULL, 0, NULL }
+};
 
 /**
  * get_handler_arg_len:
@@ -138,19 +139,19 @@ const HandlerItem g_handler_items[] = { { MONO_HANDLER_ATEXIT_WAIT_KEYPRESS, MON
 static size_t
 get_next_handler_arg_len (const char *handlers)
 {
-	assert (handlers != NULL);
+    assert (handlers != NULL);
 
-	size_t current_len = 0;
-	const char *handler = strchr (handlers, MONO_HANDLER_DELIMITER);
-	if (handler != NULL) {
-		// Get length of next handler arg.
-		current_len = (handler - handlers);
-	} else {
-		// Consume rest as length of next handler arg.
-		current_len = strlen (handlers);
-	}
+    size_t current_len = 0;
+    const char *handler = strchr (handlers, MONO_HANDLER_DELIMITER);
+    if (handler != NULL) {
+        // Get length of next handler arg.
+        current_len = (handler - handlers);
+    } else {
+        // Consume rest as length of next handler arg.
+        current_len = strlen (handlers);
+    }
 
-	return current_len;
+    return current_len;
 }
 
 /**
@@ -165,76 +166,76 @@ get_next_handler_arg_len (const char *handlers)
 static gboolean
 install_custom_handler (const char *handlers, size_t *handler_arg_len)
 {
-	gboolean result = FALSE;
+    gboolean result = FALSE;
 
-	assert (handlers != NULL);
-	assert (handler_arg_len);
+    assert (handlers != NULL);
+    assert (handler_arg_len);
 
-	*handler_arg_len = get_next_handler_arg_len (handlers);
-	for (int current_item = 0; current_item < G_N_ELEMENTS (g_handler_items); ++current_item) {
-		const HandlerItem * handler_item = &g_handler_items [current_item];
+    *handler_arg_len = get_next_handler_arg_len (handlers);
+    for (int current_item = 0; current_item < G_N_ELEMENTS (g_handler_items); ++current_item) {
+        const HandlerItem * handler_item = &g_handler_items [current_item];
 
-		if (handler_item->cmd == NULL)
-			continue;
+        if (handler_item->cmd == NULL)
+            continue;
 
-		if (*handler_arg_len == handler_item->cmd_len && strncmp (handlers, handler_item->cmd, *handler_arg_len) == 0) {
-			assert (handler_item->handler != NULL);
-			handler_item->handler ();
-			result = TRUE;
-			break;
-		}
-	}
-	return result;
+        if (*handler_arg_len == handler_item->cmd_len && strncmp (handlers, handler_item->cmd, *handler_arg_len) == 0) {
+            assert (handler_item->handler != NULL);
+            handler_item->handler ();
+            result = TRUE;
+            break;
+        }
+    }
+    return result;
 }
 
 void
 mono_runtime_install_handlers (void)
 {
 #ifndef MONO_CROSS_COMPILE
-	win32_seh_init();
-	win32_seh_set_handler(SIGFPE, mono_sigfpe_signal_handler);
-	win32_seh_set_handler(SIGILL, mono_crashing_signal_handler);
-	win32_seh_set_handler(SIGSEGV, mono_sigsegv_signal_handler);
-	if (mini_debug_options.handle_sigint)
-		win32_seh_set_handler(SIGINT, mono_sigint_signal_handler);
+    win32_seh_init();
+    win32_seh_set_handler(SIGFPE, mono_sigfpe_signal_handler);
+    win32_seh_set_handler(SIGILL, mono_crashing_signal_handler);
+    win32_seh_set_handler(SIGSEGV, mono_sigsegv_signal_handler);
+    if (mini_debug_options.handle_sigint)
+        win32_seh_set_handler(SIGINT, mono_sigint_signal_handler);
 #endif
 }
 
 gboolean
 mono_runtime_install_custom_handlers (const char *handlers)
 {
-	gboolean result = FALSE;
+    gboolean result = FALSE;
 
-	assert (handlers != NULL);
-	while (*handlers != '\0') {
-		size_t handler_arg_len = 0;
+    assert (handlers != NULL);
+    while (*handlers != '\0') {
+        size_t handler_arg_len = 0;
 
-		result = install_custom_handler (handlers, &handler_arg_len);
-		handlers += handler_arg_len;
+        result = install_custom_handler (handlers, &handler_arg_len);
+        handlers += handler_arg_len;
 
-		if (*handlers == MONO_HANDLER_DELIMITER)
-			handlers++;
-		if (!result)
-			break;
-	}
+        if (*handlers == MONO_HANDLER_DELIMITER)
+            handlers++;
+        if (!result)
+            break;
+    }
 
-	return result;
+    return result;
 }
 
 void
 mono_runtime_install_custom_handlers_usage (void)
 {
-	fprintf (stdout,
-		 "Custom Handlers:\n"
-		 "   --handlers=HANDLERS            Enable handler support, HANDLERS is a comma\n"
-		 "                                  separated list of available handlers to install.\n"
-		 "\n"
+    fprintf (stdout,
+             "Custom Handlers:\n"
+             "   --handlers=HANDLERS            Enable handler support, HANDLERS is a comma\n"
+             "                                  separated list of available handlers to install.\n"
+             "\n"
 #if G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT)
-		 "HANDLERS is composed of:\n"
-		 "    atexit-waitkeypress           Install an atexit handler waiting for a keypress\n"
-		 "                                  before exiting process.\n");
+             "HANDLERS is composed of:\n"
+             "    atexit-waitkeypress           Install an atexit handler waiting for a keypress\n"
+             "                                  before exiting process.\n");
 #else
-		 "No handlers supported on current platform.\n");
+             "No handlers supported on current platform.\n");
 #endif /* G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT) */
 }
 
@@ -242,20 +243,20 @@ void
 mono_runtime_cleanup_handlers (void)
 {
 #ifndef MONO_CROSS_COMPILE
-	win32_seh_cleanup();
+    win32_seh_cleanup();
 #endif
 }
 
 void
 mono_init_native_crash_info (void)
 {
-	return;
+    return;
 }
 
 void
 mono_cleanup_native_crash_info (void)
 {
-	return;
+    return;
 }
 
 #if G_HAVE_API_SUPPORT (HAVE_CLASSIC_WINAPI_SUPPORT | HAVE_UWP_WINAPI_SUPPORT)
@@ -268,23 +269,23 @@ mono_cleanup_native_crash_info (void)
 gboolean
 MONO_SIG_HANDLER_SIGNATURE (mono_chain_signal)
 {
-	/* Set to FALSE to indicate that vectored exception handling should continue to look for handler */
-	MONO_SIG_HANDLER_GET_INFO ()->handled = FALSE;
-	return TRUE;
+    /* Set to FALSE to indicate that vectored exception handling should continue to look for handler */
+    MONO_SIG_HANDLER_GET_INFO ()->handled = FALSE;
+    return TRUE;
 }
 
 #ifndef MONO_CROSS_COMPILE
 void
 mono_dump_native_crash_info (const char *signal, MonoContext *mctx, MONO_SIG_HANDLER_INFO_TYPE *info)
 {
-	//TBD
+    //TBD
 }
 
 void
 mono_post_native_crash_handler (const char *signal, MonoContext *mctx, MONO_SIG_HANDLER_INFO_TYPE *info, gboolean crash_chaining)
 {
-	if (!crash_chaining)
-		abort ();
+    if (!crash_chaining)
+        abort ();
 }
 #endif /* !MONO_CROSS_COMPILE */
 #endif /* G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT | HAVE_UWP_WINAPI_SUPPORT) */
@@ -296,175 +297,175 @@ static HANDLE g_timer_main_thread = INVALID_HANDLE_VALUE;
 static VOID
 thread_timer_expired (HANDLE thread)
 {
-	CONTEXT context;
+    CONTEXT context;
 
-	context.ContextFlags = CONTEXT_CONTROL;
-	if (GetThreadContext (thread, &context)) {
-		guchar *ip;
+    context.ContextFlags = CONTEXT_CONTROL;
+    if (GetThreadContext (thread, &context)) {
+        guchar *ip;
 
 #ifdef _WIN64
-		ip = (guchar *) context.Rip;
+        ip = (guchar *) context.Rip;
 #else
-		ip = (guchar *) context.Eip;
+        ip = (guchar *) context.Eip;
 #endif
 
-		MONO_PROFILER_RAISE (sample_hit, (ip, &context));
-	}
+        MONO_PROFILER_RAISE (sample_hit, (ip, &context));
+    }
 }
 
 static VOID CALLBACK
 timer_event_proc (UINT uID, UINT uMsg, DWORD_PTR dwUser, DWORD_PTR dw1, DWORD_PTR dw2)
 {
-	thread_timer_expired ((HANDLE)dwUser);
+    thread_timer_expired ((HANDLE)dwUser);
 }
 
 static VOID
 stop_profiler_timer_event (void)
 {
-	if (g_timer_event != 0) {
+    if (g_timer_event != 0) {
 
-		timeKillEvent (g_timer_event);
-		g_timer_event = 0;
-	}
+        timeKillEvent (g_timer_event);
+        g_timer_event = 0;
+    }
 
-	if (g_timer_main_thread != INVALID_HANDLE_VALUE) {
+    if (g_timer_main_thread != INVALID_HANDLE_VALUE) {
 
-		CloseHandle (g_timer_main_thread);
-		g_timer_main_thread = INVALID_HANDLE_VALUE;
-	}
+        CloseHandle (g_timer_main_thread);
+        g_timer_main_thread = INVALID_HANDLE_VALUE;
+    }
 }
 
 static VOID
 start_profiler_timer_event (void)
 {
-	g_return_if_fail (g_timer_main_thread == INVALID_HANDLE_VALUE && g_timer_event == 0);
+    g_return_if_fail (g_timer_main_thread == INVALID_HANDLE_VALUE && g_timer_event == 0);
 
-	TIMECAPS timecaps;
+    TIMECAPS timecaps;
 
-	if (timeGetDevCaps (&timecaps, sizeof (timecaps)) != TIMERR_NOERROR)
-		return;
+    if (timeGetDevCaps (&timecaps, sizeof (timecaps)) != TIMERR_NOERROR)
+        return;
 
-	g_timer_main_thread = OpenThread (READ_CONTROL | THREAD_GET_CONTEXT, FALSE, GetCurrentThreadId ());
-	if (g_timer_main_thread == NULL)
-		return;
+    g_timer_main_thread = OpenThread (READ_CONTROL | THREAD_GET_CONTEXT, FALSE, GetCurrentThreadId ());
+    if (g_timer_main_thread == NULL)
+        return;
 
-	if (timeBeginPeriod (1) != TIMERR_NOERROR)
-		return;
+    if (timeBeginPeriod (1) != TIMERR_NOERROR)
+        return;
 
-	g_timer_event = timeSetEvent (1, 0, (LPTIMECALLBACK)timer_event_proc, (DWORD_PTR)g_timer_main_thread, TIME_PERIODIC | TIME_KILL_SYNCHRONOUS);
-	if (g_timer_event == 0) {
-		timeEndPeriod (1);
-		return;
-	}
+    g_timer_event = timeSetEvent (1, 0, (LPTIMECALLBACK)timer_event_proc, (DWORD_PTR)g_timer_main_thread, TIME_PERIODIC | TIME_KILL_SYNCHRONOUS);
+    if (g_timer_event == 0) {
+        timeEndPeriod (1);
+        return;
+    }
 }
 
 void
 mono_runtime_setup_stat_profiler (void)
 {
-	start_profiler_timer_event ();
-	return;
+    start_profiler_timer_event ();
+    return;
 }
 
 void
 mono_runtime_shutdown_stat_profiler (void)
 {
-	stop_profiler_timer_event ();
-	return;
+    stop_profiler_timer_event ();
+    return;
 }
 
 gboolean
 mono_setup_thread_context(DWORD thread_id, MonoContext *mono_context)
 {
-	HANDLE handle;
-	CONTEXT context;
+    HANDLE handle;
+    CONTEXT context;
 
-	g_assert (thread_id != GetCurrentThreadId ());
+    g_assert (thread_id != GetCurrentThreadId ());
 
-	handle = OpenThread (THREAD_ALL_ACCESS, FALSE, thread_id);
-	g_assert (handle);
+    handle = OpenThread (THREAD_ALL_ACCESS, FALSE, thread_id);
+    g_assert (handle);
 
-	context.ContextFlags = CONTEXT_INTEGER | CONTEXT_FLOATING_POINT | CONTEXT_CONTROL;
+    context.ContextFlags = CONTEXT_INTEGER | CONTEXT_FLOATING_POINT | CONTEXT_CONTROL;
 
-	if (!GetThreadContext (handle, &context)) {
-		CloseHandle (handle);
-		return FALSE;
-	}
+    if (!GetThreadContext (handle, &context)) {
+        CloseHandle (handle);
+        return FALSE;
+    }
 
-	memset (mono_context, 0, sizeof (MonoContext));
-	mono_sigctx_to_monoctx (&context, mono_context);
+    memset (mono_context, 0, sizeof (MonoContext));
+    mono_sigctx_to_monoctx (&context, mono_context);
 
-	CloseHandle (handle);
-	return TRUE;
+    CloseHandle (handle);
+    return TRUE;
 }
 #endif /* G_HAVE_API_SUPPORT(HAVE_CLASSIC_WINAPI_SUPPORT) */
 
 gboolean
 mono_thread_state_init_from_handle (MonoThreadUnwindState *tctx, MonoThreadInfo *info, void *sigctx)
 {
-	tctx->valid = FALSE;
-	tctx->unwind_data [MONO_UNWIND_DATA_DOMAIN] = NULL;
-	tctx->unwind_data [MONO_UNWIND_DATA_LMF] = NULL;
-	tctx->unwind_data [MONO_UNWIND_DATA_JIT_TLS] = NULL;
+    tctx->valid = FALSE;
+    tctx->unwind_data [MONO_UNWIND_DATA_DOMAIN] = NULL;
+    tctx->unwind_data [MONO_UNWIND_DATA_LMF] = NULL;
+    tctx->unwind_data [MONO_UNWIND_DATA_JIT_TLS] = NULL;
 
-	if (sigctx == NULL) {
-		DWORD id = mono_thread_info_get_tid (info);
-		mono_setup_thread_context (id, &tctx->ctx);
-	} else {
-		g_assert (((CONTEXT *)sigctx)->ContextFlags & CONTEXT_INTEGER);
-		g_assert (((CONTEXT *)sigctx)->ContextFlags & CONTEXT_CONTROL);
-		mono_sigctx_to_monoctx (sigctx, &tctx->ctx);
-	}
+    if (sigctx == NULL) {
+        DWORD id = mono_thread_info_get_tid (info);
+        mono_setup_thread_context (id, &tctx->ctx);
+    } else {
+        g_assert (((CONTEXT *)sigctx)->ContextFlags & CONTEXT_INTEGER);
+        g_assert (((CONTEXT *)sigctx)->ContextFlags & CONTEXT_CONTROL);
+        mono_sigctx_to_monoctx (sigctx, &tctx->ctx);
+    }
 
-	/* mono_set_jit_tls () sets this */
-	void *jit_tls = mono_thread_info_tls_get (info, TLS_KEY_JIT_TLS);
-	/* SET_APPDOMAIN () sets this */
-	void *domain = mono_thread_info_tls_get (info, TLS_KEY_DOMAIN);
+    /* mono_set_jit_tls () sets this */
+    void *jit_tls = mono_thread_info_tls_get (info, TLS_KEY_JIT_TLS);
+    /* SET_APPDOMAIN () sets this */
+    void *domain = mono_thread_info_tls_get (info, TLS_KEY_DOMAIN);
 
-	/*Thread already started to cleanup, can no longer capture unwind state*/
-	if (!jit_tls || !domain)
-		return FALSE;
+    /*Thread already started to cleanup, can no longer capture unwind state*/
+    if (!jit_tls || !domain)
+        return FALSE;
 
-	/*
-	 * The current LMF address is kept in a separate TLS variable, and its hard to read its value without
-	 * arch-specific code. But the address of the TLS variable is stored in another TLS variable which
-	 * can be accessed through MonoThreadInfo.
-	 */
-	/* mono_set_lmf_addr () sets this */
-	MonoLMF *lmf = NULL;
-	MonoLMF **addr = (MonoLMF**)mono_thread_info_tls_get (info, TLS_KEY_LMF_ADDR);
-	if (addr)
-		lmf = *addr;
+    /*
+     * The current LMF address is kept in a separate TLS variable, and its hard to read its value without
+     * arch-specific code. But the address of the TLS variable is stored in another TLS variable which
+     * can be accessed through MonoThreadInfo.
+     */
+    /* mono_set_lmf_addr () sets this */
+    MonoLMF *lmf = NULL;
+    MonoLMF **addr = (MonoLMF**)mono_thread_info_tls_get (info, TLS_KEY_LMF_ADDR);
+    if (addr)
+        lmf = *addr;
 
-	tctx->unwind_data [MONO_UNWIND_DATA_DOMAIN] = domain;
-	tctx->unwind_data [MONO_UNWIND_DATA_JIT_TLS] = jit_tls;
-	tctx->unwind_data [MONO_UNWIND_DATA_LMF] = lmf;
-	tctx->valid = TRUE;
+    tctx->unwind_data [MONO_UNWIND_DATA_DOMAIN] = domain;
+    tctx->unwind_data [MONO_UNWIND_DATA_JIT_TLS] = jit_tls;
+    tctx->unwind_data [MONO_UNWIND_DATA_LMF] = lmf;
+    tctx->valid = TRUE;
 
-	return TRUE;
+    return TRUE;
 }
 
 BOOL
 mono_win32_runtime_tls_callback (HMODULE module_handle, DWORD reason, LPVOID reserved, MonoWin32TLSCallbackType callback_type)
 {
-	if (!mono_win32_handle_tls_callback_type (callback_type))
-		return TRUE;
+    if (!mono_win32_handle_tls_callback_type (callback_type))
+        return TRUE;
 
-	if (!mono_gc_dllmain (module_handle, reason, reserved))
-		return FALSE;
+    if (!mono_gc_dllmain (module_handle, reason, reserved))
+        return FALSE;
 
-	switch (reason)
-	{
-	case DLL_PROCESS_ATTACH:
-		mono_install_runtime_load (mini_init);
-		break;
-	case DLL_PROCESS_DETACH:
-		if (coree_module_handle)
-			FreeLibrary (coree_module_handle);
-		break;
-	case DLL_THREAD_DETACH:
-		mono_thread_info_detach ();
-		break;
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        mono_install_runtime_load (mini_init);
+        break;
+    case DLL_PROCESS_DETACH:
+        if (coree_module_handle)
+            FreeLibrary (coree_module_handle);
+        break;
+    case DLL_THREAD_DETACH:
+        mono_thread_info_detach ();
+        break;
 
-	}
-	return TRUE;
+    }
+    return TRUE;
 }
