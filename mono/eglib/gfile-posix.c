f9c5bbaba20d88fe955ae502f75476d5bd1673ef
Restyled.io <commits@restyled.io>
2020-04-17 18:37:16 +0000

    Restyled by clang-format

--- mono/eglib/gfile-posix.c
+++ mono/eglib/gfile-posix.c
@@ -29,19 +29,19 @@
 #include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-#include <fcntl.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #ifdef _MSC_VER
 #include <direct.h>
 #endif
 #ifdef G_OS_WIN32
-int mkstemp (char *tmp_template);
+int mkstemp(char *tmp_template);
 #endif
 
 #ifndef O_LARGEFILE
@@ -49,138 +49,139 @@ int mkstemp (char *tmp_template);
 #else
 #define OPEN_FLAGS (O_RDONLY | O_LARGEFILE)
 #endif
-gboolean
-g_file_get_contents (const gchar *filename, gchar **contents, gsize *length, GError **gerror)
-{
-    gchar *str;
-    int fd;
-    struct stat st;
-    long offset;
-    int nread;
+gboolean g_file_get_contents(const gchar *filename, gchar **contents,
+                             gsize *length, GError **gerror) {
+  gchar *str;
+  int fd;
+  struct stat st;
+  long offset;
+  int nread;
 
-    g_return_val_if_fail (filename != NULL, FALSE);
-    g_return_val_if_fail (contents != NULL, FALSE);
-    g_return_val_if_fail (gerror == NULL || *gerror == NULL, FALSE);
+  g_return_val_if_fail(filename != NULL, FALSE);
+  g_return_val_if_fail(contents != NULL, FALSE);
+  g_return_val_if_fail(gerror == NULL || *gerror == NULL, FALSE);
 
-    *contents = NULL;
-    if (length)
-        *length = 0;
+  *contents = NULL;
+  if (length)
+    *length = 0;
 
-    fd = open (filename, OPEN_FLAGS);
-    if (fd == -1) {
-        if (gerror != NULL) {
-            int err = errno;
-            *gerror = g_error_new (G_LOG_DOMAIN, g_file_error_from_errno (err), "Error opening file");
-        }
-        return FALSE;
+  fd = open(filename, OPEN_FLAGS);
+  if (fd == -1) {
+    if (gerror != NULL) {
+      int err = errno;
+      *gerror = g_error_new(G_LOG_DOMAIN, g_file_error_from_errno(err),
+                            "Error opening file");
     }
+    return FALSE;
+  }
 
-    if (fstat (fd, &st) != 0) {
-        if (gerror != NULL) {
-            int err = errno;
-            *gerror = g_error_new (G_LOG_DOMAIN, g_file_error_from_errno (err), "Error in fstat()");
-        }
-        close (fd);
-        return FALSE;
+  if (fstat(fd, &st) != 0) {
+    if (gerror != NULL) {
+      int err = errno;
+      *gerror = g_error_new(G_LOG_DOMAIN, g_file_error_from_errno(err),
+                            "Error in fstat()");
     }
+    close(fd);
+    return FALSE;
+  }
 
-    str = g_malloc (st.st_size + 1);
-    offset = 0;
-    do {
-        nread = read (fd, str + offset, st.st_size - offset);
-        if (nread > 0) {
-            offset += nread;
-        }
-    } while ((nread > 0 && offset < st.st_size) || (nread == -1 && errno == EINTR));
+  str = g_malloc(st.st_size + 1);
+  offset = 0;
+  do {
+    nread = read(fd, str + offset, st.st_size - offset);
+    if (nread > 0) {
+      offset += nread;
+    }
+  } while ((nread > 0 && offset < st.st_size) ||
+           (nread == -1 && errno == EINTR));
 
-    close (fd);
+  close(fd);
 
 #ifdef G_OS_WIN32
-    // Windows defaults to O_TEXT for opened files, meaning that st_size can be larger than
-    // what's actually read into str due to new line conversion.
-    g_assert (offset <= st.st_size);
-    str [offset] = '\0';
-    if (length)
-        *length = offset;
+  // Windows defaults to O_TEXT for opened files, meaning that st_size can be
+  // larger than what's actually read into str due to new line conversion.
+  g_assert(offset <= st.st_size);
+  str[offset] = '\0';
+  if (length)
+    *length = offset;
 #else
-    str [st.st_size] = '\0';
-    if (length) {
-        *length = st.st_size;
-    }
+  str[st.st_size] = '\0';
+  if (length) {
+    *length = st.st_size;
+  }
 #endif
 
-    *contents = str;
-    return TRUE;
+  *contents = str;
+  return TRUE;
 }
 
-gint
-g_file_open_tmp (const gchar *tmpl, gchar **name_used, GError **gerror)
-{
-    const static gchar *default_tmpl = ".XXXXXX";
-    gchar *t;
-    gint fd;
-    size_t len;
+gint g_file_open_tmp(const gchar *tmpl, gchar **name_used, GError **gerror) {
+  const static gchar *default_tmpl = ".XXXXXX";
+  gchar *t;
+  gint fd;
+  size_t len;
 
-    g_return_val_if_fail (gerror == NULL || *gerror == NULL, -1);
+  g_return_val_if_fail(gerror == NULL || *gerror == NULL, -1);
 
-    if (tmpl == NULL)
-        tmpl = default_tmpl;
+  if (tmpl == NULL)
+    tmpl = default_tmpl;
 
-    if (strchr (tmpl, G_DIR_SEPARATOR) != NULL) {
-        if (gerror) {
-            *gerror = g_error_new (G_LOG_DOMAIN, 24, "Template should not have any " G_DIR_SEPARATOR_S);
-        }
-        return -1;
+  if (strchr(tmpl, G_DIR_SEPARATOR) != NULL) {
+    if (gerror) {
+      *gerror = g_error_new(G_LOG_DOMAIN, 24,
+                            "Template should not have any " G_DIR_SEPARATOR_S);
     }
+    return -1;
+  }
 
-    len = strlen (tmpl);
-    if (len < 6 || strcmp (tmpl + len - 6, "XXXXXX")) {
-        if (gerror) {
-            *gerror = g_error_new (G_LOG_DOMAIN, 24, "Template should end with XXXXXX");
-        }
-        return -1;
+  len = strlen(tmpl);
+  if (len < 6 || strcmp(tmpl + len - 6, "XXXXXX")) {
+    if (gerror) {
+      *gerror =
+          g_error_new(G_LOG_DOMAIN, 24, "Template should end with XXXXXX");
     }
+    return -1;
+  }
 
-    t = g_build_filename (g_get_tmp_dir (), tmpl, (const char*)NULL);
+  t = g_build_filename(g_get_tmp_dir(), tmpl, (const char *)NULL);
 
-    fd = mkstemp (t);
+  fd = mkstemp(t);
 
-    if (fd == -1) {
-        if (gerror) {
-            int err = errno;
-            *gerror = g_error_new (G_LOG_DOMAIN, g_file_error_from_errno (err), "Error in mkstemp()");
-        }
-        g_free (t);
-        return -1;
+  if (fd == -1) {
+    if (gerror) {
+      int err = errno;
+      *gerror = g_error_new(G_LOG_DOMAIN, g_file_error_from_errno(err),
+                            "Error in mkstemp()");
     }
+    g_free(t);
+    return -1;
+  }
 
-    if (name_used) {
-        *name_used = t;
-    } else {
-        g_free (t);
-    }
-    return fd;
+  if (name_used) {
+    *name_used = t;
+  } else {
+    g_free(t);
+  }
+  return fd;
 }
 
-gchar *
-g_get_current_dir (void)
-{
-    int s = 32;
-    char *buffer = NULL, *r;
-    gboolean fail;
+gchar *g_get_current_dir(void) {
+  int s = 32;
+  char *buffer = NULL, *r;
+  gboolean fail;
 
-    do {
-        buffer = g_realloc (buffer, s);
-        r = getcwd (buffer, s);
-        fail = (r == NULL && errno == ERANGE);
-        if (fail) {
-            s <<= 1;
-        }
-    } while (fail);
+  do {
+    buffer = g_realloc(buffer, s);
+    r = getcwd(buffer, s);
+    fail = (r == NULL && errno == ERANGE);
+    if (fail) {
+      s <<= 1;
+    }
+  } while (fail);
 
-    /* On amd64 sometimes the bottom 32-bits of r == the bottom 32-bits of buffer
-     * but the top 32-bits of r have overflown to 0xffffffff (seriously, getcwd
-     * so we return the buffer here since it has a pointer to the valid string
-     */
-    return buffer;
+  /* On amd64 sometimes the bottom 32-bits of r == the bottom 32-bits of buffer
+   * but the top 32-bits of r have overflown to 0xffffffff (seriously, getcwd
+   * so we return the buffer here since it has a pointer to the valid string
+   */
+  return buffer;
 }
