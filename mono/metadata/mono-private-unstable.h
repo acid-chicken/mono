c7e0eb2678664fda7cc5e656446b4c05b8385bc5
Restyled.io <commits@restyled.io>
2020-04-19 08:36:20 +0000

    Restyled by clang-format

--- mono/metadata/mono-private-unstable.h
+++ mono/metadata/mono-private-unstable.h
@@ -3,11 +3,10 @@
  *
  * Private unstable APIs.
  *
- * WARNING: The declarations and behavior of functions in this header are NOT STABLE and can be modified or removed at
- * any time.
+ * WARNING: The declarations and behavior of functions in this header are NOT
+ * STABLE and can be modified or removed at any time.
  *
  */
-
 
 #ifndef __MONO_METADATA_MONO_PRIVATE_UNSTABLE_H__
 #define __MONO_METADATA_MONO_PRIVATE_UNSTABLE_H__
@@ -16,10 +15,15 @@
 
 typedef MonoGCHandle MonoAssemblyLoadContextGCHandle;
 
-MONO_API MONO_RT_EXTERNAL_ONLY
-MonoAssembly *mono_assembly_load_full_alc (MonoAssemblyLoadContextGCHandle alc_gchandle, MonoAssemblyName *aname, const char *basedir, MonoImageOpenStatus *status);
+MONO_API MONO_RT_EXTERNAL_ONLY MonoAssembly *
+mono_assembly_load_full_alc(MonoAssemblyLoadContextGCHandle alc_gchandle,
+                            MonoAssemblyName *aname, const char *basedir,
+                            MonoImageOpenStatus *status);
 
-typedef MonoAssembly * (*MonoAssemblyPreLoadFuncV3) (MonoAssemblyLoadContextGCHandle *alc_gchandle, MonoAssemblyName *aname, char **assemblies_path, gpointer user_data, MonoError *error);
-void mono_install_assembly_preload_hook_v3 (MonoAssemblyPreLoadFuncV3 func, gpointer user_data, gboolean append);
+typedef MonoAssembly *(*MonoAssemblyPreLoadFuncV3)(
+    MonoAssemblyLoadContextGCHandle *alc_gchandle, MonoAssemblyName *aname,
+    char **assemblies_path, gpointer user_data, MonoError *error);
+void mono_install_assembly_preload_hook_v3(MonoAssemblyPreLoadFuncV3 func,
+                                           gpointer user_data, gboolean append);
 
 #endif /*__MONO_METADATA_MONO_PRIVATE_UNSTABLE_H__*/
