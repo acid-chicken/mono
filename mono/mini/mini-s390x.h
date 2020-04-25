4b74da5a371081da877db8c26ae2158a4841bc5e
Restyled.io <commits@restyled.io>
2020-04-25 17:08:10 +0000

    Restyled by clang-format

--- mono/mini/mini-s390x.h
+++ mono/mini/mini-s390x.h
@@ -19,62 +19,61 @@
 /*-------------------------------------------*/
 
 struct MonoLMF {
-    gpointer    previous_lmf;
-    gpointer    lmf_addr;
-    MonoMethod *method;
-    gulong      ebp;
-    gulong      eip;
-    gulong	    pregs[6];
-    gulong	    gregs[16];
-    gdouble     fregs[16];
+  gpointer previous_lmf;
+  gpointer lmf_addr;
+  MonoMethod *method;
+  gulong ebp;
+  gulong eip;
+  gulong pregs[6];
+  gulong gregs[16];
+  gdouble fregs[16];
 };
 
 typedef struct MonoCompileArch {
-    int         bkchain_reg;
-    uint32_t    used_fp_regs;
-    int	    fpSize;
+  int bkchain_reg;
+  uint32_t used_fp_regs;
+  int fpSize;
 } MonoCompileArch;
 
-typedef struct
-{
-    void *prev;
-    void *unused[5];
-    void *regs[8];
-    void *return_address;
+typedef struct {
+  void *prev;
+  void *unused[5];
+  void *regs[8];
+  void *return_address;
 } MonoS390StackFrame;
 
-#define MONO_ARCH_SIGSEGV_ON_ALTSTACK			1
-#define MONO_ARCH_EMULATE_LCONV_TO_R8_UN 		1
-#define MONO_ARCH_NO_EMULATE_LONG_MUL_OPTS		1
-#define MONO_ARCH_NO_EMULATE_LONG_SHIFT_OPS		1
-#define MONO_ARCH_HAVE_IS_INT_OVERFLOW  		1
-#define MONO_ARCH_NEED_DIV_CHECK			1
-#define MONO_ARCH_SIGNAL_STACK_SIZE 			256*1024
-#define MONO_ARCH_HAVE_DECOMPOSE_OPTS 			1
-#define MONO_ARCH_IMT_REG				s390_r9
-#define MONO_ARCH_VTABLE_REG				S390_FIRST_ARG_REG
-#define MONO_ARCH_RGCTX_REG				MONO_ARCH_IMT_REG
-#define MONO_ARCH_SOFT_DEBUG_SUPPORTED			1
-#define MONO_ARCH_HAVE_CONTEXT_SET_INT_REG		1
-#define MONO_ARCH_USE_SIGACTION 			1
-#define MONO_ARCH_GC_MAPS_SUPPORTED			1
-#define MONO_ARCH_GSHARED_SUPPORTED			1
-#define MONO_ARCH_MONITOR_ENTER_ADJUSTMENT		1
-#define MONO_ARCH_HAVE_INVALIDATE_METHOD		1
-#define MONO_ARCH_HAVE_OP_GENERIC_CLASS_INIT		1
-#define MONO_ARCH_HAVE_SETUP_ASYNC_CALLBACK		1
-#define MONO_ARCH_HAVE_TRACK_FPREGS			1
-#define MONO_ARCH_HAVE_OPTIMIZED_DIV			1
-#define MONO_ARCH_HAVE_OP_TAILCALL_MEMBASE		1
-#define MONO_ARCH_HAVE_OP_TAILCALL_REG			1
+#define MONO_ARCH_SIGSEGV_ON_ALTSTACK 1
+#define MONO_ARCH_EMULATE_LCONV_TO_R8_UN 1
+#define MONO_ARCH_NO_EMULATE_LONG_MUL_OPTS 1
+#define MONO_ARCH_NO_EMULATE_LONG_SHIFT_OPS 1
+#define MONO_ARCH_HAVE_IS_INT_OVERFLOW 1
+#define MONO_ARCH_NEED_DIV_CHECK 1
+#define MONO_ARCH_SIGNAL_STACK_SIZE 256 * 1024
+#define MONO_ARCH_HAVE_DECOMPOSE_OPTS 1
+#define MONO_ARCH_IMT_REG s390_r9
+#define MONO_ARCH_VTABLE_REG S390_FIRST_ARG_REG
+#define MONO_ARCH_RGCTX_REG MONO_ARCH_IMT_REG
+#define MONO_ARCH_SOFT_DEBUG_SUPPORTED 1
+#define MONO_ARCH_HAVE_CONTEXT_SET_INT_REG 1
+#define MONO_ARCH_USE_SIGACTION 1
+#define MONO_ARCH_GC_MAPS_SUPPORTED 1
+#define MONO_ARCH_GSHARED_SUPPORTED 1
+#define MONO_ARCH_MONITOR_ENTER_ADJUSTMENT 1
+#define MONO_ARCH_HAVE_INVALIDATE_METHOD 1
+#define MONO_ARCH_HAVE_OP_GENERIC_CLASS_INIT 1
+#define MONO_ARCH_HAVE_SETUP_ASYNC_CALLBACK 1
+#define MONO_ARCH_HAVE_TRACK_FPREGS 1
+#define MONO_ARCH_HAVE_OPTIMIZED_DIV 1
+#define MONO_ARCH_HAVE_OP_TAILCALL_MEMBASE 1
+#define MONO_ARCH_HAVE_OP_TAILCALL_REG 1
 
-#define S390_STACK_ALIGNMENT		 8
-#define S390_FIRST_ARG_REG 		s390_r2
-#define S390_LAST_ARG_REG 		s390_r6
-#define S390_FIRST_FPARG_REG 		s390_f0
-#define S390_LAST_FPARG_REG 		s390_f6
+#define S390_STACK_ALIGNMENT 8
+#define S390_FIRST_ARG_REG s390_r2
+#define S390_LAST_ARG_REG s390_r6
+#define S390_FIRST_FPARG_REG s390_f0
+#define S390_LAST_FPARG_REG s390_f6
 
-#define S390_FP_SAVE_MASK		0xf0
+#define S390_FP_SAVE_MASK 0xf0
 
 /*===============================================*/
 /* Definitions used by mini-codegen.c            */
@@ -103,16 +102,18 @@ typedef struct
 
 #define MONO_ARCH_USE_FPSTACK FALSE
 
-#define MONO_ARCH_INST_FIXED_REG(desc) ((desc == 'o') ? s390_r2 : 		\
-					((desc == 'g') ? s390_f0 : 		\
-					((desc == 'A') ? S390_FIRST_ARG_REG : -1)))
+#define MONO_ARCH_INST_FIXED_REG(desc)                                         \
+  ((desc == 'o')                                                               \
+       ? s390_r2                                                               \
+       : ((desc == 'g') ? s390_f0                                              \
+                        : ((desc == 'A') ? S390_FIRST_ARG_REG : -1)))
 
-#define MONO_ARCH_INST_IS_FLOAT(desc)  ((desc == 'f') || (desc == 'g'))
+#define MONO_ARCH_INST_IS_FLOAT(desc) ((desc == 'f') || (desc == 'g'))
 
 #define MONO_ARCH_INST_SREG2_MASK(ins) (0)
 
 #define MONO_ARCH_INST_IS_REGPAIR(desc) (0)
-#define MONO_ARCH_INST_REGPAIR_REG2(desc,hr) -1
+#define MONO_ARCH_INST_REGPAIR_REG2(desc, hr) -1
 
 #define MONO_ARCH_IS_GLOBAL_IREG(reg) 0
 
@@ -123,9 +124,9 @@ typedef struct
 /* SIMD Related Definitions                      */
 /*-----------------------------------------------*/
 
-#define MONO_MAX_XREGS			31
-#define MONO_ARCH_CALLEE_XREGS		0x0
-#define MONO_ARCH_CALLEE_SAVED_XREGS	0x0
+#define MONO_MAX_XREGS 31
+#define MONO_ARCH_CALLEE_XREGS 0x0
+#define MONO_ARCH_CALLEE_SAVED_XREGS 0x0
 
 // Does the ABI have a volatile non-parameter register, so tailcall
 // can pass context to generics or interfaces?
@@ -134,24 +135,28 @@ typedef struct
 /*-----------------------------------------------*/
 /* Macros used to generate instructions          */
 /*-----------------------------------------------*/
-#define S390_OFFSET(b, t)	(guchar *) ((guint64) (b) - (guint64) (t))
-#define S390_RELATIVE(b, t)     (guchar *) ((((guint64) (b) - (guint64) (t))) / 2)
+#define S390_OFFSET(b, t) (guchar *)((guint64)(b) - (guint64)(t))
+#define S390_RELATIVE(b, t) (guchar *)((((guint64)(b) - (guint64)(t))) / 2)
 
-#define CODEPTR(c, o) (o) = (short *) ((guint64) c - 2)
-#define PTRSLOT(c, o) *(o) = (short) ((guint64) c - (guint64) (o) + 2)/2
+#define CODEPTR(c, o) (o) = (short *)((guint64)c - 2)
+#define PTRSLOT(c, o) *(o) = (short)((guint64)c - (guint64)(o) + 2) / 2
 
-#define S390_CC_EQ			8
-#define S390_ALIGN(v, a)	(((a) > 0 ? (((v) + ((a) - 1)) & ~((a) - 1)) : (v)))
+#define S390_CC_EQ 8
+#define S390_ALIGN(v, a) (((a) > 0 ? (((v) + ((a)-1)) & ~((a)-1)) : (v)))
 
-#define MONO_INIT_CONTEXT_FROM_FUNC(ctx,func) do {			\
-		MonoS390StackFrame *sframe;				\
-		__asm__ volatile("lgr   %0,15" : "=r" (sframe));	\
-		MONO_CONTEXT_SET_BP ((ctx), sframe->prev);		\
-		MONO_CONTEXT_SET_SP ((ctx), sframe->prev);		\
-		MONO_CONTEXT_SET_IP ((ctx), func);			\
-	} while (0)
+#define MONO_INIT_CONTEXT_FROM_FUNC(ctx, func)                                 \
+  do {                                                                         \
+    MonoS390StackFrame *sframe;                                                \
+    __asm__ volatile("lgr   %0,15" : "=r"(sframe));                            \
+    MONO_CONTEXT_SET_BP((ctx), sframe->prev);                                  \
+    MONO_CONTEXT_SET_SP((ctx), sframe->prev);                                  \
+    MONO_CONTEXT_SET_IP((ctx), func);                                          \
+  } while (0)
 
-#define MONO_ARCH_INIT_TOP_LMF_ENTRY(lmf) do { (lmf)->ebp = -1; } while (0)
+#define MONO_ARCH_INIT_TOP_LMF_ENTRY(lmf)                                      \
+  do {                                                                         \
+    (lmf)->ebp = -1;                                                           \
+  } while (0)
 
 /*------------------------------------------------------------------*/
 /*                                                                  */
@@ -161,14 +166,12 @@ typedef struct
 /*                                                                  */
 /*------------------------------------------------------------------*/
 
-static void inline
-s390_patch_rel (guchar *code, guint64 target)
-{
-    guint32 *offset = (guint32 *) code;
+static void inline s390_patch_rel(guchar *code, guint64 target) {
+  guint32 *offset = (guint32 *)code;
 
-    if (target != 0) {
-        *offset = (guint32) target;
-    }
+  if (target != 0) {
+    *offset = (guint32)target;
+  }
 }
 
 /*========================= End of Function ========================*/
@@ -181,14 +184,12 @@ s390_patch_rel (guchar *code, guint64 target)
 /*                                                                  */
 /*------------------------------------------------------------------*/
 
-static void inline
-s390_patch_addr (guchar *code, guint64 target)
-{
-    guint64 *offset = (guint64 *) code;
+static void inline s390_patch_addr(guchar *code, guint64 target) {
+  guint64 *offset = (guint64 *)code;
 
-    if (target != 0) {
-        *offset = target;
-    }
+  if (target != 0) {
+    *offset = target;
+  }
 }
 
 /*========================= End of Function ========================*/
@@ -201,37 +202,36 @@ s390_patch_addr (guchar *code, guint64 target)
 /*                                                                  */
 /*------------------------------------------------------------------*/
 
-#define restoreLMF(code, frame_reg, stack_usage) do			\
-{									\
-	int lmfOffset = 0;						\
-									\
-	s390_lgr (code, s390_r13, frame_reg);				\
-									\
-	lmfOffset = stack_usage -  sizeof(MonoLMF);			\
-									\
-	/*-------------------------------------------------*/		\
-	/* r13 = my lmf					   */		\
-	/*-------------------------------------------------*/		\
-	s390_aghi (code, s390_r13, lmfOffset);				\
-									\
-	/*-------------------------------------------------*/		\
-	/* r6 = &jit_tls->lmf				   */		\
-	/*-------------------------------------------------*/		\
-	s390_lg  (code, s390_r6, 0, s390_r13, 				\
-		  G_STRUCT_OFFSET(MonoLMF, lmf_addr));			\
-									\
-	/*-------------------------------------------------*/		\
-	/* r0 = lmf.previous_lmf			   */		\
-	/*-------------------------------------------------*/		\
-	s390_lg  (code, s390_r0, 0, s390_r13, 				\
-		  G_STRUCT_OFFSET(MonoLMF, previous_lmf));		\
-									\
-	/*-------------------------------------------------*/		\
-	/* jit_tls->lmf = previous_lmf			   */		\
-	/*-------------------------------------------------*/		\
-	s390_lg  (code, s390_r13, 0, s390_r6, 0);			\
-	s390_stg (code, s390_r0, 0, s390_r6, 0);			\
-} while (0)
+#define restoreLMF(code, frame_reg, stack_usage)                               \
+  do {                                                                         \
+    int lmfOffset = 0;                                                         \
+                                                                               \
+    s390_lgr(code, s390_r13, frame_reg);                                       \
+                                                                               \
+    lmfOffset = stack_usage - sizeof(MonoLMF);                                 \
+                                                                               \
+    /*-------------------------------------------------*/                      \
+    /* r13 = my lmf					   */                                                  \
+    /*-------------------------------------------------*/                      \
+    s390_aghi(code, s390_r13, lmfOffset);                                      \
+                                                                               \
+    /*-------------------------------------------------*/                      \
+    /* r6 = &jit_tls->lmf				   */                                             \
+    /*-------------------------------------------------*/                      \
+    s390_lg(code, s390_r6, 0, s390_r13, G_STRUCT_OFFSET(MonoLMF, lmf_addr));   \
+                                                                               \
+    /*-------------------------------------------------*/                      \
+    /* r0 = lmf.previous_lmf			   */                                           \
+    /*-------------------------------------------------*/                      \
+    s390_lg(code, s390_r0, 0, s390_r13,                                        \
+            G_STRUCT_OFFSET(MonoLMF, previous_lmf));                           \
+                                                                               \
+    /*-------------------------------------------------*/                      \
+    /* jit_tls->lmf = previous_lmf			   */                                     \
+    /*-------------------------------------------------*/                      \
+    s390_lg(code, s390_r13, 0, s390_r6, 0);                                    \
+    s390_stg(code, s390_r0, 0, s390_r6, 0);                                    \
+  } while (0)
 
 /*========================= End of Function ========================*/
 
--- mono/utils/mono-hwcap-vars.h
+++ mono/utils/mono-hwcap-vars.h
@@ -4,7 +4,7 @@
 
 #include "config.h"
 
-#if defined (TARGET_ARM)
+#if defined(TARGET_ARM)
 
 MONO_HWCAP_VAR(arm_is_v5)
 MONO_HWCAP_VAR(arm_is_v6)
@@ -15,15 +15,15 @@ MONO_HWCAP_VAR(arm_has_vfp3_d16)
 MONO_HWCAP_VAR(arm_has_thumb)
 MONO_HWCAP_VAR(arm_has_thumb2)
 
-#elif defined (TARGET_ARM64)
+#elif defined(TARGET_ARM64)
 
 // Nothing here yet.
 
-#elif defined (TARGET_MIPS)
+#elif defined(TARGET_MIPS)
 
 // Nothing here yet.
 
-#elif defined (TARGET_POWERPC) || defined (TARGET_POWERPC64)
+#elif defined(TARGET_POWERPC) || defined(TARGET_POWERPC64)
 
 MONO_HWCAP_VAR(ppc_has_icache_snoop)
 MONO_HWCAP_VAR(ppc_is_isa_2x)
@@ -32,7 +32,7 @@ MONO_HWCAP_VAR(ppc_is_isa_64)
 MONO_HWCAP_VAR(ppc_has_move_fpr_gpr)
 MONO_HWCAP_VAR(ppc_has_multiple_ls_units)
 
-#elif defined (TARGET_RISCV)
+#elif defined(TARGET_RISCV)
 
 MONO_HWCAP_VAR(riscv_has_stdext_a)
 MONO_HWCAP_VAR(riscv_has_stdext_b)
@@ -48,7 +48,7 @@ MONO_HWCAP_VAR(riscv_has_stdext_q)
 MONO_HWCAP_VAR(riscv_has_stdext_t)
 MONO_HWCAP_VAR(riscv_has_stdext_v)
 
-#elif defined (TARGET_S390X)
+#elif defined(TARGET_S390X)
 
 MONO_HWCAP_VAR(s390x_has_fpe)
 MONO_HWCAP_VAR(s390x_has_vec)
@@ -60,11 +60,11 @@ MONO_HWCAP_VAR(s390x_has_mie3)
 MONO_HWCAP_VAR(s390x_has_gs)
 MONO_HWCAP_VAR(s390x_has_vef2)
 
-#elif defined (TARGET_SPARC) || defined (TARGET_SPARC64)
+#elif defined(TARGET_SPARC) || defined(TARGET_SPARC64)
 
 MONO_HWCAP_VAR(sparc_is_v9)
 
-#elif defined (TARGET_X86) || defined (TARGET_AMD64)
+#elif defined(TARGET_X86) || defined(TARGET_AMD64)
 
 MONO_HWCAP_VAR(x86_is_xen)
 MONO_HWCAP_VAR(x86_has_cmov)
